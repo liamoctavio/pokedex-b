b4f63e4057a71acfd03805abb2d8799a40b7efa2
liamoctavio <ocmolina47@gmail.com>
2024-12-02 04:02:47 -0300

    pokedex details

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import index, PokemonList, search_pokemon, autocomplete
+from .views import index, PokemonList, search_pokemon, autocomplete, pokemon_detail
 
 urlpatterns = [
     path('', index, name='index'),
     path('api/pokemon/', PokemonList.as_view(), name='pokemon_list'),
     path('search/', search_pokemon, name='search_pokemon'),
     path('autocomplete/', autocomplete, name='autocomplete'),
+    path('pokemon/<str:pokemon_name>/', pokemon_detail, name='pokemon_detail'),
+
 
 
 ]
